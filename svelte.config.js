eaaf2107d56394b838257b88bcb07c028a262ae7
Jason Luo <jason_z_luo@manulife.com>
2024-06-02 20:09:28 +0800

    0.2

--- svelte.config.js
+++ svelte.config.js
@@ -15,10 +15,9 @@ const config = {
 			assets: 'build',
 			fallback: 'index.html'
 		}),
-		// paths: {
-		// 	base: '/'
-		// },
-		appDir: 'servant'
+		paths: {
+			base: '/servant'
+		}
 	},
 	onwarn: (warning, handler) => {
 		const { code, _ } = warning;
